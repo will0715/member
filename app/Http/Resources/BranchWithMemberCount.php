8aa51e69b632c9d471b98e2d053cd698b47a9e6e
your <w67890w67890@gmail.com>
2020-08-07 19:55:52 +0800

    調整回傳report時間問題

---
+++ app/Http/Resources/BranchWithMemberCount.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BranchWithMemberCount extends JsonResource
+{
+    
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $resource = $this;
+
+        return [
+            'id' => $resource->id,
+            'code' => $resource->code,
+            'name' => $resource->name,
+            'store_name' => $resource->store_name,
+            'telphone' => $resource->telphone,
+            'fax' => $resource->fax,
+            'note' => $resource->note,
+            'zipcode' => $resource->zipcode,
+            'state' => $resource->state,
+            'city' => $resource->city,
+            'county' => $resource->county,
+            'address' => $resource->address,
+            'latitude' => $resource->latitude,
+            'longitude' => $resource->longitude,
+            'remark' => $resource->remark,
+            'opening_times' => $resource->opening_times,
+            'is_independent' => $resource->is_independent,
+            'created_at' => $resource->created_at,
+            'register_members_count' => $resource->register_members_count,
+            'register_members' => Member::collection($resource->whenLoaded('registerMembers'))
+        ];
+    }
+}
